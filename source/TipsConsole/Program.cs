9613c49af31819253c631259c9f05b1ac3e0540c
walt-instructor <staffauthor@outlook.com>
2022-06-10 15:24:47 -0700

    Format code

--- source/TipsConsole/Program.cs
+++ source/TipsConsole/Program.cs
@@ -1,14 +1,14 @@
 ﻿namespace TipsConsole {
-  internal class Program {
-    static void Main(string[] args) {
-      Console.WriteLine("Visualize IEnumerables while debugging.");
+	internal class Program {
+		static void Main(string[] args) {
+			Console.WriteLine("Visualize IEnumerables while debugging.");
 
-      var source = new DataLib.CardSource();
-      var oddNumber = new List<int> { 1, 3,5,7,9};
-      var mCards = source.MonsterCards;
-      var aCards = source.AlienCards;
+			var source = new DataLib.CardSource();
+			var oddNumber = new List<int> { 1, 3, 5, 7, 9 };
+			var mCards = source.MonsterCards;
+			var aCards = source.AlienCards;
 
-      Console.ReadLine();
-    }
-  }
+			Console.ReadLine();
+		}
+	}
 }
