8a36d757d8d6e08f82185b661e164cdc2fca3b2d
Walt Ritscher <waltritscher@outlook.com>
2022-06-10 14:17:14 -0700

    Add integer example

--- source/TipsConsole/Program.cs
+++ source/TipsConsole/Program.cs
@@ -1,7 +1,14 @@
 ﻿namespace TipsConsole {
   internal class Program {
     static void Main(string[] args) {
-      Console.WriteLine("Hello, World!");
+      Console.WriteLine("Visualize IEnumerables while debugging.");
+
+      var source = new DataLib.CardSource();
+      var oddNumber = new List<int> { 1, 3,5,7,9};
+      var mCards = source.MonsterCards;
+      var aCards = source.AlienCards;
+
+      Console.ReadLine();
     }
   }
 }
