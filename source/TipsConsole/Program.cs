529d6d24412f4d099c2e7a5d960ad48c9a9f9bb3
Walt Ritscher <waltritscher@outlook.com>
2022-06-11 13:48:51 -0700

    add example xml comments

--- source/TipsConsole/Program.cs
+++ source/TipsConsole/Program.cs
@@ -1,7 +1,7 @@
 ﻿namespace TipsConsole {
   internal class Program {
     static void Main(string[] args) {
-      Console.WriteLine("Visualize IEnumerables while debugging.");
+      Console.WriteLine("Tip 11 - XML Code comments");
 
       var source = new DataLib.CardSource();
       var oddNumber = new List<int> { 1, 3,5,7,9};
