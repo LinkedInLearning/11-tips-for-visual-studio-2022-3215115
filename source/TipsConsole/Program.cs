9cb0991d87ecde39ce4c9acff6cf515000ef99af
Walt Ritscher <waltritscher@outlook.com>
2022-06-11 12:36:35 -0700

    Rename solution folder

--- source/TipsConsole/Program.cs
+++ source/TipsConsole/Program.cs
@@ -1,7 +1,8 @@
 ﻿namespace TipsConsole {
   internal class Program {
     static void Main(string[] args) {
-      Console.WriteLine("Visualize IEnumerables while debugging.");
+     
+      Console.WriteLine("Tip 10 - Track Active Document");
 
       var source = new DataLib.CardSource();
       var oddNumber = new List<int> { 1, 3,5,7,9};
